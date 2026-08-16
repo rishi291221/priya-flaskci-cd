3c974998f9e4d6ecc360a5f1ebb7381131b537fe
rishi291221 <darshini.nka@gmail.com>
2026-08-16 16:29:39 +0530

    flask CI-CD files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
@@ -58,6 +58,26 @@ def update_student(student_id):
 def delete_student(student_id):
     mongo.db.students.delete_one({"_id": ObjectId(student_id)})
     return redirect(url_for('index'))
+
+
+# Health check
+@app.route('/health')
+def health():
+    try:
+        mongo.cx.admin.command("ping")
+
+        return jsonify({
+            "status": "healthy",
+            "mongodb": "connected"
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "status": "unhealthy",
+            "mongodb": "disconnected",
+            "error": str(e)
+        }), 503
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=5000)
